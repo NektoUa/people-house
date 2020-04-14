99410b6cb799d680d374246c5fb6f1f229e360b1
NektoUa <illyabarkov@gmail.com>
2020-04-14 14:39:26 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Sum = () => {
+  let n = 4;
+  let m = 5;
+  let sum = n + m;
+  return sum;
+}
+
+const Ibra = {
+  name: "IbraMun",
+  last: "Tadeuш"
+}
+
+function searchIbra() {
+  return `We love ${Ibra.name}, but never forgot his lastname ${Ibra.last}`;
+}
+
+const App = function dfrp() {
+  const aram = <p>Dou Go</p>;
+  const sum = 56 - 46;
   return (
-    <div className="App">
+    <div className="App" >
+      <p>Never mind where are you from</p>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <p>!1! Never mind where are you from</p>
+        <h2>
+          {searchIbra()}
+        </h2>
+        <img src={logo} class="App-logo" alt="logo" />
+        {sum}
+        {aram}
+        <br />
+        <Sum />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit code <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
