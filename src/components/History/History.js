845bd8aa42dbc86c0a86159f0b4debeb69a3eaba
NektoUa <illyabarkov@gmail.com>
2020-06-06 14:17:10 +0300

    css style

--- src/components/History/History.js
+++ src/components/History/History.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import main from './Main.module.css';
+import main from './Main.module.css';
 
 export default class History extends Component {
     constructor(props) {
@@ -20,11 +20,11 @@ export default class History extends Component {
         const body = this.state.isOpen && <section>{about.text}</section>
 
         return (
-            <div id="begin" >
+            <div id="begin" className={main.text}>
                 <h2>
-                    {about.period}
-                    <button type="button" onClick={this.handleClick}>{this.state.isOpen ? 'Згорнути' : 'Розгорнути'}</button>
-                </h2>
+                    {about.period}</h2>
+                <button type="button" onClick={this.handleClick} className={main.button} >{this.state.isOpen ? 'Згорнути ' : 'Читати'}</button>
+
                 {body}
 
             </div >
--- src/components/literature/Literature.js
+++ src/components/literature/Literature.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 import main from './Main.module.css';
 import Form from './form/form'
 
@@ -15,22 +14,31 @@ class Literature extends Component {
     handleAddBook = (data) => {
         const nextBook = [...this.state.book, data]
         // this.props.addBook(data)
-        this.setState({ book: nextBook })
+        this.setState({ book: nextBook, isOpen: true })
 
     }
 
+    handleClick = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
+
     render() {
+        const addForm = this.state.isOpen && <div><Form onAddBook={this.handleAddBook} addBook={this.props.addBook} /></div>;
         const books = this.state.book;
         const bookList = books.map((elem) => {
             return <li key={elem.author}>{elem.author} {elem.title} / {elem.publish}. - {elem.year}</li>
         });
+
         return (
             <div className={main.lit} id="begin">
                 <h2>Де можна прочитати про пам'ятку</h2>
                 <ol>
                     {bookList}
                 </ol>
-                <Form onAddBook={this.handleAddBook} addBook={this.props.addBook} />
+                <a href> <h2 onClick={this.handleClick}>Додати літературу</h2></a>
+                {addForm}
             </div >
         );
     }
